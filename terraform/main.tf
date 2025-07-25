d1061326f6cfa56853522c52af37928163aaef85
Wonka1124 <Profession123122@gmail.com>
2025-07-25 02:33:22 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0c02fb55956c7d316" # Ubuntu 20.04 LTS для eu-central-1
+  ami           = "ami-0c02fb55956c7d316" # Ubuntu 20.04 LTS
   instance_type = "t2.micro"
 
   tags = {
